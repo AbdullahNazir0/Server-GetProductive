61a5c423564827e11c3d98c15b05efab7f3f3d5b
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2025-03-26 17:27:08 +0500

    Fixed cookie authentication issue

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -69,6 +69,7 @@ const loginUser = asyncHandler(async (req, res) => {
     .cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000
     })
     .json(
