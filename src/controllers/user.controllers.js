1f67724db3c4c7fbabb3814e948721ff63daff77
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2025-04-06 04:30:56 +0500

    Fixed logout cookie bug

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -95,6 +95,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     .cookie('token', '', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 0
     })
     .json(
